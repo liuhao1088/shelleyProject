f300c0185fe3c6d817af042e7a25fadd8edead3c
liuhao1088 <1350569467@qq.com>
2020-11-20 16:41:15 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    url:[
+      'https://img12.360buyimg.com/ddimg/jfs/t1/125749/18/18992/88955/5fb77bf7Ef28aef02/9969721a65f206f0.png',
+      'https://img11.360buyimg.com/ddimg/jfs/t1/149801/9/15214/84373/5fb77f5fE6703b3e3/cfc63e812c95bd6a.png',
+      'https://img11.360buyimg.com/ddimg/jfs/t1/131154/14/16544/101703/5fb77f7bEf9dae1d6/a2e338acd1913cb2.png',
+      'https://img11.360buyimg.com/ddimg/jfs/t1/122546/28/19388/99801/5fb77f8aE5bcec9c5/dd12a83a33c44745.png',
+      'https://img14.360buyimg.com/ddimg/jfs/t1/145889/37/14730/88666/5fb77fa0Eddfcea31/7132ccdce4f86f6d.png',
+      'https://img12.360buyimg.com/ddimg/jfs/t1/121576/23/19358/91213/5fb77fafEddf3b1e2/56368d754af746aa.png'
+    ]
   },
 
   /**
