034cd6f3e6b817df7c1bd98153896e85e2acfd24
liuhao1088 <1350569467@qq.com>
2020-12-18 19:08:20 +0800

    更新

--- pages/groupActivities/groupActivities.js
+++ pages/groupActivities/groupActivities.js
@@ -1,23 +1,46 @@
 // pages/groupActivities/groupActivities.js
+var clickindex = new Array();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    startTime: '2020-12-17 11:32',//活动开始时间
-    endTime: '2020-12-17 11:32',//活动截止时间
-    productList:[
-      {
-        "name": '',
-        "price": '',
-        "people":'',
-        "time":'时间',
-        "showView":false
-      }
-    ],//活动商品
-    
-   
+    startTime: '2020-12-17 11:32', //活动开始时间
+    endTime: '2020-12-17 11:32', //活动截止时间
+    productList: [{
+      "name": '请选择想要体验的商品',
+      "price": '',
+      "people": '',
+      "time": '时间',
+      "showView": false
+    }], //活动商品
+    checkbox: [{
+      value: 0,
+      name: '如影系列-R1',
+      checked: false,
+    }, {
+      value: 1,
+      name: '如影系列-R2',
+      checked: false,
+    }, {
+      value: 2,
+      name: '斑马系列-B2',
+      checked: false,
+    }, {
+      value: 3,
+      name: '斑马系列-B3',
+      checked: false,
+    }, {
+      value: 4,
+      name: '双色系列-S3',
+      checked: false,
+    }, {
+      value: 5,
+      name: '猎豹系列-T3',
+      checked: false,
+    }], //商品选择
+    index:0
   },
   //获取活动开始时间
   changeStartTime(e) {
@@ -37,23 +60,26 @@ Page({
 
   //获取分钟
   timeChange(e) {
+    let productList =  this.data.productList
     var index = e.currentTarget.dataset.idx; //获取当前索引
-    console.log(index)
-    this.data.productList[index].time = e.detail.value
+    // console.log(index)
+    productList[index].time = e.detail.value;
+    this.setData({
+      productList,
+    })
   },
 
   //添加活动商品
-  onAdd(e){
-    console.log(e)
+  onAdd(e) {
     let productList = this.data.productList;
     let index = e.currentTarget.dataset.idx;
-    console.log(index)
+    // console.log(index)
     let newData = {
-      "name": '',
+      "name": '请选择想要体验的商品',
       "price": '',
-      "people":'',
-      "time":'时间',
-      "showView":false
+      "people": '',
+      "time": '时间',
+      "showView": false
     };
     if (productList.length >= 6) {
       wx.showToast({
@@ -61,24 +87,81 @@ Page({
         icon: 'none',
         duration: 2000
       })
-       return;
-     }
+      return;
+    }
     productList.push(newData);
-    console.log(this.data.productList[index].showView)
-    this.data.productList[index].showView  =  !this.data.productList[index].showView;
-    console.log(this.data.productList[index].showView)
+    // console.log(this.data.productList[index].showView)
+    this.data.productList[index].showView = !this.data.productList[index].showView;
+    // console.log(this.data.productList[index].showView)
     this.setData({
       productList,
     })
   },
 
   //获取input的值
-  bindChanguser(e){
+  bindChanguser(e) {
+    let productList = this.data.productList;
     var index = e.currentTarget.dataset.idx; //获取当前索引
-    var type = e.currentTarget.id;//状态
-    console.log(index)
-    this.data.productList[index][type] = e.detail.value
-    console.log(this.data.productList[index][type])
+    var type = e.currentTarget.id; //状态
+    // console.log(index)
+    productList[index][type] = e.detail.value
+    // console.log(productList[index][type])
+  },
+
+  //失去焦点赋值给input
+  blurChanguser(e){
+    let productList = this.data.productList;
+    var index = e.currentTarget.dataset.idx; //获取当前索引
+    var type = e.currentTarget.id; //状态
+    console.log(index);
+    productList[index][type] = e.detail.value;
+    this.setData({
+      [productList[index][type]]:e.detail.value,
+    })
+  },
+  showModal(e) {
+    // console.log(e.currentTarget.dataset.idx)
+    this.setData({
+      modalName: e.currentTarget.dataset.target,
+      index:e.currentTarget.dataset.idx
+    })
+  },
+  hideModal(e) {
+    let index = this.data.index;
+    let productList = this.data.productList;
+    let nameList = [];
+    let items = this.data.checkbox;
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].checked == true) {
+        nameList.push(items[i].name);
+      }
+    }
+    if (nameList == '') {
+      productList[index].name = ['请选择想要体验的商品']
+    }
+    productList[index].name = nameList
+    this.setData({
+      modalName: null,
+      productList:productList
+    })
+    // console.log(productList)
+  },
+  chooseCheckbox(e) {
+    let items = this.data.checkbox;
+    let values = e.currentTarget.dataset.value;
+    // console.log(values)
+    for (let i = 0; i < items.length; ++i) {
+      if (items[i].value == values) {
+        console.log(items[i].value);
+        items[i].checked = true;
+      }else{
+        items[i].checked = false;
+      }
+    }
+    this.setData({
+      checkbox: items,
+    })
+
   },
 
   /**
