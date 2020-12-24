c8a5c8894825a507ca9d6592e91ab222ec220571
liuhao1088 <1350569467@qq.com>
2020-12-24 14:12:07 +0800

    更新

--- pages/storeAppointment/storeAppointment.js
+++ pages/storeAppointment/storeAppointment.js
@@ -1,5 +1,5 @@
 // pages/storeAppointment/storeAppointment.js
-var util=require('../../utils/util.js')
+var util = require('../../utils/util.js')
 var timer;
 Page({
 
@@ -9,12 +9,17 @@ Page({
   data: {
     startTime: '2020-12-17 11:32',
     checkbox: [],
-    nameList:['请选择想要体验的商品，可多选'],
-    data:'',firstLoading:true,isShow:false,
-    reservation:'',ind:0
+    nameList: ['请选择想要体验的商品，可多选'],
+    data: '',
+    firstLoading: true,
+    isShow: false,
+    reservation: '',
+    ind: 0
   },
-  changeStartTime(e){
-    this.setData({ startTime: e.detail.value})
+  changeStartTime(e) {
+    this.setData({
+      startTime: e.detail.value
+    })
   },
   showModal(e) {
     this.setData({
@@ -24,32 +29,36 @@ Page({
   hideModal(e) {
     let nameList = [];
     let items = this.data.checkbox;
-    for (let i = 0;i < items.length;i++) {
-      if(items[i].checked == true){
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].checked == true) {
         nameList.push(items[i].name);
       }
     }
-    if(nameList == ''){
-      nameList = ['请选择想要体验的商品，可多选'] 
-    }else{
-      this.setData({isShow:true})
+    if (nameList == '') {
+      nameList = ['请选择想要体验的商品，可多选']
+    } else {
+      this.setData({
+        isShow: true
+      })
     }
     this.setData({
       modalName: null,
       nameList
     })
-    if(wx.getStorageSync('phone')){
-      this.setData({isShow:false})
+    if (wx.getStorageSync('phone')) {
+      this.setData({
+        isShow: false
+      })
     }
     console.log(nameList)
   },
   ChooseCheckbox(e) {
     let items = this.data.checkbox;
     let ind = e.currentTarget.dataset.index;
-    items[ind].checked=!items[ind].checked;
+    items[ind].checked = !items[ind].checked;
     this.setData({
       checkbox: items,
-      ind:ind
+      ind: ind
     })
   },
 
@@ -57,148 +66,162 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if(options.data){
-      let data=JSON.parse(options.data)
+    if (options.data) {
+      let data = JSON.parse(options.data)
       console.log(data)
-      if(data.reservation.length!==0){
-        this.setData({reservation:'already'})
+      if (data.reservation.length !== 0) {
+        this.setData({
+          reservation: 'already'
+        })
       }
-      this.setData({data:data})
+      this.setData({
+        data: data
+      })
     }
-    this.setData({startTime:util.formatTime(new Date())})
-    var that=this;
-    wx.cloud.callFunction({name:'showwares'}).then(res=>{
-      let data=res.result;
-      for(let i=0;i<data.length;i++){
-        data[i].checked=false;
-        if(i+1==data.length) that.setData({checkbox:data})
+    this.setData({
+      startTime: util.formatTime(new Date())
+    })
+    var that = this;
+    wx.cloud.callFunction({
+      name: 'showwares'
+    }).then(res => {
+      let data = res.result;
+      for (let i = 0; i < data.length; i++) {
+        data[i].checked = false;
+        if (i + 1 == data.length) that.setData({
+          checkbox: data
+        })
       }
     })
     wx.cloud.callFunction({
-      name:'login'
-    }).then(res=>console.log(res))
+      name: 'login'
+    }).then(res => console.log(res))
   },
-  openLocation:function(){
-    var that=this;
+  openLocation: function () {
+    var that = this;
     wx.openLocation({
       latitude: that.data.data.shop[0].lat,
       longitude: that.data.data.shop[0].lon,
     })
   },
-  getPhoneNumber:function(e){
-    var that=this;
+  getPhoneNumber: function (e) {
+    var that = this;
     console.log(e)
-    if(e.detail.errMsg=="getPhoneNumber:ok"){
+    if (e.detail.errMsg == "getPhoneNumber:ok") {
       wx.showLoading({
         title: '授权中',
       })
       wx.cloud.callFunction({
-        name:'decode',
+        name: 'decode',
         data: {
           weRunData: wx.cloud.CloudID(e.detail.cloudID),
         }
-      }).then(res=>{
+      }).then(res => {
         that.setData({
           phone: res.result,
         })
-        let phone=res.result;
-        if(!wx.getStorageSync('phone')){
+        let phone = res.result;
+        if (!wx.getStorageSync('phone')) {
           wx.setStorageSync('phone', res.result)
           wx.cloud.database().collection('user').where({
-            _openid:wx.getStorageSync('userInfo')._openid
-          }).get().then(res=>{
+            _openid: wx.getStorageSync('userInfo')._openid
+          }).get().then(res => {
             console.log(res)
-            if(!res.data[0].phone){
+            if (!res.data[0].phone) {
               wx.cloud.callFunction({
-                name:'recordUpdate',
-                data:{
-                  collection:'user',
-                  where:{_openid:wx.getStorageSync('userInfo')._openid},
-                  updateData:{phone:phone}
+                name: 'recordUpdate',
+                data: {
+                  collection: 'user',
+                  where: {
+                    _openid: wx.getStorageSync('userInfo')._openid
+                  },
+                  updateData: {
+                    phone: phone
+                  }
                 }
-              }).then(res=>{
+              }).then(res => {
                 console.log(res)
               })
             }
           })
         }
-        let userInfo=wx.getStorageSync('userInfo')
-        userInfo.phone=phone;
+        let userInfo = wx.getStorageSync('userInfo')
+        userInfo.phone = phone;
         wx.setStorageSync('userInfo', userInfo)
         wx.hideLoading()
         wx.showToast({
           title: '授权成功',
-          icon:'success'
+          icon: 'success'
         })
         if (timer) clearTimeout(timer);
-        timer= setTimeout(async res=>{
-          that.add(phone,userInfo);
-        },500)
-      }).catch(error=>{
+        timer = setTimeout(async res => {
+          that.add(phone, userInfo);
+        }, 500)
+      }).catch(error => {
         console.log(error);
         wx.hideLoading()
         wx.showToast({
           title: '授权失败',
-          icon:'none'
+          icon: 'none'
         })
       })
-      
+
     }
   },
-  async submit(){
-    var that=this;
+  async submit() {
+    var that = this;
     console.log(that.data.nameList)
-    if(that.data.nameList[0]=="请选择想要体验的商品，可多选"){
+    if (that.data.nameList[0] == "请选择想要体验的商品，可多选") {
       wx.showToast({
         title: '请选择商品',
-        icon:'none'
+        icon: 'none'
       })
     } else {
       if (timer) clearTimeout(timer);
-      timer= setTimeout(async res=>{
-      let phone=wx.getStorageSync('phone')
-      let userInfo=wx.getStorageSync('userInfo')
-        that.add(phone,userInfo);
-      },500)
+      timer = setTimeout(async res => {
+        let phone = wx.getStorageSync('phone')
+        let userInfo = wx.getStorageSync('userInfo')
+        that.add(phone, userInfo);
+      }, 500)
     }
   },
-  add(phone,userInfo){
-    var that=this;
+  add(phone, userInfo) {
+    var that = this;
     wx.showLoading({
       title: '预约中，请稍等',
     })
     wx.cloud.callFunction({
-      name:'recordAdd',
-      data:{
-        collection:'reservation',
-        addData:{
-          _openid:userInfo._openid,
+      name: 'recordAdd',
+      data: {
+        collection: 'reservation',
+        addData: {
+          _openid: userInfo._openid,
           creation_date: util.formatTime(new Date()),
           phone: phone,
-          shop_code:that.data.data.shop[0].shop_code,
-          act_id:that.data.data._id,
-          act_code:that.data.data.act_code,
-          shopping:that.data.nameList,
-          time:that.data.startTime,
-          timestamp:Date.parse(that.data.startTime.replace(/-/g, '/')) / 1000,
-          user:userInfo.nickName,
+          shop_code: that.data.data.shop[0].shop_code,
+          act_id: that.data.data._id,
+          act_code: that.data.data.act_code,
+          shopping: that.data.nameList,
+          time: that.data.startTime,
+          timestamp: Date.parse(that.data.startTime.replace(/-/g, '/')) / 1000,
+          user: userInfo.nickName,
           creation_timestamp: Date.parse(util.formatTime(new Date()).replace(/-/g, '/')) / 1000,
         }
       }
-    }).then(res=>{
+    }).then(res => {
       console.log(res)
       wx.hideLoading()
       wx.showToast({
         title: '预约成功',
-        icon:'success',
-        duration:2000
+        icon: 'success',
+        duration: 2000
       })
       wx.setStorageSync('refreshData', that.data.data)
-      setTimeout(()=>{
+      setTimeout(() => {
         wx.navigateBack({
           delta: 0,
         })
-      },1500)
+      }, 1500)
     })
   },
   /**
@@ -212,8 +235,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if(wx.getStorageSync('phone')){
-      this.setData({isShow:false})
+    if (wx.getStorageSync('phone')) {
+      this.setData({
+        isShow: false
+      })
     }
   },
 
