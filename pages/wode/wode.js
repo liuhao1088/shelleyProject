212adb22849f76a75b7ff5f39a63e64162be98d8
liuhao1088 <1350569467@qq.com>
2021-01-12 19:03:53 +0800

    +修改我的

--- pages/wode/wode.js
+++ pages/wode/wode.js
@@ -1,5 +1,5 @@
 var app = getApp();
-var util=require('../../utils/util')
+var util = require('../../utils/util')
 Page({
 
   /**
@@ -12,6 +12,49 @@ Page({
       nickName: '用户名',
       avatarUrl: ''
     },
+    carLight: [{
+        id: 0,
+        name: '是',
+        checked: false,
+      },
+      {
+        id: 1,
+        name: '否',
+        checked: false,
+      },
+    ],
+    claim:[
+      {
+        id: 0,
+        name: '产品设计',
+        checked: false,
+      },
+      {
+        id: 1,
+        name: '功率射程',
+        checked: false,
+      },
+      {
+        id: 2,
+        name: '温控散热',
+        checked: false,
+      },
+      {
+        id: 3,
+        name: '性价比',
+        checked: false,
+      },
+      {
+        id: 4,
+        name: '专车兼容',
+        checked: false,
+      },
+      {
+        id: 5,
+        name: '售后服务',
+        checked: false,
+      },
+    ],
     checkbox: [{
         id: 0,
         name: '行车记录仪',
@@ -41,10 +84,14 @@ Page({
         checked: false,
       }
     ],
-    userInfo:{type:'driver',nickName:'用户名',avatarUrl:''},
-    newMsg:0,
-    prize:false,
-    posterImg:[
+    userInfo: {
+      type: 'driver',
+      nickName: '用户名',
+      avatarUrl: ''
+    },
+    newMsg: 0,
+    prize: false,
+    posterImg: [
       'https://img14.360buyimg.com/ddimg/jfs/t1/166688/37/1598/104678/5ff7cf5bEb402e7cf/f01242efb00089f2.png',
       'https://img14.360buyimg.com/ddimg/jfs/t1/166688/37/1598/104678/5ff7cf5bEb402e7cf/f01242efb00089f2.png',
       'https://img14.360buyimg.com/ddimg/jfs/t1/166688/37/1598/104678/5ff7cf5bEb402e7cf/f01242efb00089f2.png'
@@ -93,9 +140,9 @@ Page({
   },
 
   toMessageCenter() {
-    if(!wx.getStorageSync('userInfo')){
+    if (!wx.getStorageSync('userInfo')) {
       this.selectComponent("#authorize").showModal();
-    }else{
+    } else {
       wx.navigateTo({
         url: '/pages/messageCenter/messageCenter',
       })
@@ -114,57 +161,84 @@ Page({
     })
   },
 
-    //弹窗
-    showModal(e) {
-      let that = this;
-      let target = e.currentTarget.dataset.target;
-      let ind = e.currentTarget.dataset.index;
-      that.setData({
-        waresInd: ind
+  //弹窗
+  showModal(e) {
+    let that = this;
+    let target = e.currentTarget.dataset.target;
+    let ind = e.currentTarget.dataset.index;
+    that.setData({
+      waresInd: ind
+    })
+    console.log(that.data.waresInd)
+    if (target === 'goGroupSuccess') {
+      wx.showLoading({
+        title: '加载中...',
       })
-      console.log(that.data.waresInd)
-      if (target === 'goGroupSuccess') {
-        wx.showLoading({
-          title: '加载中...',
-        })
-        setTimeout(function () {
-          that.setData({
-            modalName: target
-          })
-          wx.hideLoading({
-            success: (res) => {},
-          })
-        }, 1000)
-  
-      } else {
+      setTimeout(function () {
         that.setData({
           modalName: target
         })
-      }
-    },
-    hideModal(e) {
-      this.setData({
-        modalName: null,
-        transfer: false
+        wx.hideLoading({
+          success: (res) => {},
+        })
+      }, 1000)
+
+    } else {
+      that.setData({
+        modalName: target
       })
-    },
-  
-    // 多选
-    ChooseCheckbox(e) {
-      let items = this.data.checkbox;
-      let id = e.currentTarget.id;
-      console.log(id)
-      for (let i = 0; i < items.length; ++i) {
-        if (items[i].id == id) {
-          console.log(items[i].id)
-          items[i].checked = !items[i].checked;
-          break
-        }
+    }
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null,
+      transfer: false
+    })
+  },
+
+  //车灯单选
+  carLightsCheckbox(e) {
+    let carLight = this.data.carLight;
+    let ind = e.currentTarget.id;
+    for (let i in carLight) carLight[i].checked = false
+    carLight[ind].checked = true;
+    this.setData({
+      carLight
+    })
+  },
+
+  claimCheckbox(e) {
+    let claim = this.data.claim;
+    let id = e.currentTarget.id;
+    console.log(id)
+    for (let i = 0; i < claim.length; ++i) {
+      if (claim[i].id == id) {
+        console.log(claim[i].id)
+        claim[i].checked = !claim[i].checked;
+        break
       }
-      this.setData({
-        checkbox: items
-      })
-    },
+    }
+    this.setData({
+      claim
+    })
+  },
+
+  // 多选
+  ChooseCheckbox(e) {
+    let items = this.data.checkbox;
+    let id = e.currentTarget.id;
+    console.log(id)
+    for (let i = 0; i < items.length; ++i) {
+      if (items[i].id == id) {
+        console.log(items[i].id)
+        items[i].checked = !items[i].checked;
+        break
+      }
+    }
+    this.setData({
+      checkbox: items
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -184,30 +258,38 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if(wx.getStorageSync('userInfo')){
-      let userInfo =  wx.getStorageSync('userInfo');
+    if (wx.getStorageSync('userInfo')) {
+      let userInfo = wx.getStorageSync('userInfo');
       this.setData({
         userInfo
       })
       console.log(userInfo)
     }
     wx.removeStorageSync('prize')
-    var _t=this
-    switch(wx.getStorageSync('userInfo')!==''){
+    var _t = this
+    switch (wx.getStorageSync('userInfo') !== '') {
       case true:
-        wx.cloud.database().collection('message').where({_openid:app.globalData.openid,read:'unread'}).count({
-          success:function(res){
+        wx.cloud.database().collection('message').where({
+          _openid: app.globalData.openid,
+          read: 'unread'
+        }).count({
+          success: function (res) {
             _t.setData({
-              newMsg:res.total
+              newMsg: res.total
             })
           }
         })
-        if(!wx.getStorageSync('prize')){
-          wx.cloud.database().collection('coupon').where({_openid:app.globalData.openid,shop_code:'all'}).get().then(res=>{
-            let data=res.data;
-            if(data.length==0){
-              _t.setData({prize:true})
-            }else{
+        if (!wx.getStorageSync('prize')) {
+          wx.cloud.database().collection('coupon').where({
+            _openid: app.globalData.openid,
+            shop_code: 'all'
+          }).get().then(res => {
+            let data = res.data;
+            if (data.length == 0) {
+              _t.setData({
+                prize: true
+              })
+            } else {
               wx.setStorageSync('prize', data)
             }
           })
@@ -215,19 +297,19 @@ Page({
         break;
     }
   },
-  
-  toSign:function(){
-    if(!wx.getStorageSync('userInfo')){
+
+  toSign: function () {
+    if (!wx.getStorageSync('userInfo')) {
       this.selectComponent("#authorize").showModal();
     }
   },
-  getCoupon:function(){
-    var that=this;
-    let userInfo=wx.getStorageSync('userInfo')
+  getCoupon: function () {
+    var that = this;
+    let userInfo = wx.getStorageSync('userInfo')
     wx.showLoading({
       title: '领取中'
     })
-    let code='';
+    let code = '';
     for (let e = 0; e < 6; e++) {
       code += Math.floor(Math.random() * 10)
     }
@@ -238,14 +320,18 @@ Page({
         addData: {
           creation_date: util.formatTimes(new Date()),
           creation_timestamp: Date.parse(util.formatTimes(new Date()).replace(/-/g, '/')) / 1000,
-          end_date:util.nextYear(new Date()),
+          end_date: util.nextYear(new Date()),
           end_timestamp: Date.parse(util.nextYear(new Date()).replace(/-/g, '/')) / 1000,
           _openid: userInfo._openid,
-          cou_code:code,
-          act_id:'-1',
+          cou_code: code,
+          act_id: '-1',
           user: userInfo.nickName,
-          shop_code:'all',
-          shopping: {name:'全品类商品',price:'0',original_price:'100'},
+          shop_code: 'all',
+          shopping: {
+            name: '全品类商品',
+            price: '0',
+            original_price: '100'
+          },
           status: 'success'
         }
       }
@@ -253,16 +339,18 @@ Page({
       wx.hideLoading()
       wx.showToast({
         title: '领取成功',
-        icon:'success',
+        icon: 'success',
         duration: 500
       })
-      that.setData({modalName:null})
-      let device=[]
-      for(let i=0;i<that.data.checkbox.length;i++){
-        if(that.data.checkbox[i].checked==true){
+      that.setData({
+        modalName: null
+      })
+      let device = []
+      for (let i = 0; i < that.data.checkbox.length; i++) {
+        if (that.data.checkbox[i].checked == true) {
           device.push(that.data.checkbox[i].name)
         }
-        if(i+1==that.data.checkbox.length){
+        if (i + 1 == that.data.checkbox.length) {
           wx.cloud.callFunction({
             name: 'recordAdd',
             data: {
@@ -272,19 +360,25 @@ Page({
                 creation_timestamp: Date.parse(util.formatTimes(new Date()).replace(/-/g, '/')) / 1000,
                 _openid: userInfo._openid,
                 user: userInfo.nickName,
-                device:device
+                device: device
               }
             }
           }).then(res => {})
         }
       }
-      that.setData({prize:false})
-      wx.setStorageSync('prize', [{status:'success',cou_code:code,act_id:'-1'}])
+      that.setData({
+        prize: false
+      })
+      wx.setStorageSync('prize', [{
+        status: 'success',
+        cou_code: code,
+        act_id: '-1'
+      }])
       wx.showModal({
-        title:'成功领取100现金抵扣红包',
-        content:'红包已经放进我的卡券里，是否前往查看',
-        success:function(res){
-          if(res.confirm){
+        title: '成功领取100现金抵扣红包',
+        content: '红包已经放进我的卡券里，是否前往查看',
+        success: function (res) {
+          if (res.confirm) {
             wx.navigateTo({
               url: '../myCoupon/myCoupon',
             })
